2858acde75149bde0217a86212a8a3806830581c
ME-SR7 <me.sreenivas07@gmail.com>
2025-09-25 20:47:29 +0530

    Time: 11 ms (39.25%) | Memory: 17.9 MB (20.57%) - LeetSync

--- 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.py
+++ 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.py
@@ -2,6 +2,7 @@ class Solution:
     def countEven(self, num: int) -> int:
         count=0
         for i in range(1,num+1):
+            print(i)
             sum=0
             n=i
             while n>0:
@@ -24,22 +25,6 @@ class Solution:
 
 
 
-
-
-
-
-
-        # count=0
-       
-        # for i in range(1,num+1):
-        #     sum=0
-        #     x=i
-        #     while x>0:
-        #         sum +=x%10
-        #         x=x//10
-        #     if sum%2==0:
-        #         count+=1
-        # return count
 
 
         
