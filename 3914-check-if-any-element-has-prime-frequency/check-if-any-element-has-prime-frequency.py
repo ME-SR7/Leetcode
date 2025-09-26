dccdd3fe6963e731ec8d47b86c0723e8cf071b9f
ME-SR7 <me.sreenivas07@gmail.com>
2025-09-26 18:55:21 +0530

    Time: 3 ms (38.74%) | Memory: 17.8 MB (43.83%) - LeetSync

---
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.py
@@ -0,0 +1,26 @@
+class Solution:
+   
+    def checkPrimeFrequency(self, nums: List[int]) -> bool:
+        
+        dict_seen={}
+        for num in nums:
+            dict_seen[num]=1+dict_seen.get(num,0)
+       
+        for count in dict_seen.values():
+            if count<2:
+                continue
+            if count==2 or count==3:
+                return True
+            is_prime=True
+            for j in range(2,int((count**0.5)+1)):
+                if count%j==0:
+                    is_prime=False
+                    break
+            if is_prime:
+                return True        
+                
+        return False
+        
+
+         
+        
