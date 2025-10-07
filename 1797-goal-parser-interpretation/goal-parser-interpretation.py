648e9dfcb06c4154b2fb628f9a0c17c27aaf6891
ME-SR7 <me.sreenivas07@gmail.com>
2025-10-07 15:22:40 +0530

    Time: 26 ms (97.52%) | Memory: 17.7 MB (41.99%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -1,14 +1,18 @@
 class Solution:
     def interpret(self, command: str) -> str:
         output=[]
-        for i in range(len(command)):
+        i=0
+        while i < len(command):
             if command[i]=="G":
                 output.append("G")
+                i+=1
 
             elif command[i:i+2]=="()":
                 output.append("o")
+                i+=2
             elif command[i:i+4]=="(al)":
                 output.append("al")
+                i+=4
             
         return "".join(output)
         
